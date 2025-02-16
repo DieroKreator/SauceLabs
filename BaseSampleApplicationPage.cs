ec40cffeca297551a0a70631cb4599ede173fbcd
DieroKreator <qtechwired@gmail.com>
2025-02-16 17:12:48 -0300

    Add BaseSampleApplicationPage class to provide a base for page interactions with Selenium WebDriver

---
+++ BaseSampleApplicationPage.cs
@@ -0,0 +1,12 @@
+using OpenQA.Selenium;
+
+namespace SauceLabs;
+public class BaseSampleApplicationPage
+{
+    protected IWebDriver Driver { get; set; }
+
+    public BaseSampleApplicationPage(IWebDriver driver)
+    {
+        Driver = driver;
+    }
+}
