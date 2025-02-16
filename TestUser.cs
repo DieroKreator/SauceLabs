5e50f0982acec3f13eae70568858f30a7640a4e9
DieroKreator <qtechwired@gmail.com>
2025-02-16 17:08:08 -0300

    Add TestUser class to represent user details for SauceLabs testing

---
+++ TestUser.cs
@@ -0,0 +1,8 @@
+namespace SauceLabs;
+
+internal class TestUser
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public Gender GenderType { get; set; }
+}
